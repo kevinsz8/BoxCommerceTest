0aa37dbae9bdf3793c48ab71df6a5691662032ac
ksanchez91 <kesanchez@appbali.com>
2023-10-18 16:17:36 -0600

    Addinf some projects to start with the development and testing connection to local database

---
+++ ManufacturerVehicles/ManufacturerVehicles.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using ManufacturerVehicles.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace ManufacturerVehicles.DataAccess
+{
+	public class ApplicationDbContext : DbContext
+	{
+		protected readonly IConfiguration Configuration;
+		public ApplicationDbContext(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+		}
+
+		public DbSet<Customer> Customers
+		{
+			get;
+			set;
+		}
+
+	}
+}
---
+++ ManufacturerVehicles/ManufacturerVehicles.Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManufacturerVehicles.Models
+{
+	public class Customer
+	{
+		[Key]
+		public int CustomerID { get; set; }
+
+		[MaxLength(255)]
+		public string Name { get; set; }
+
+		[MaxLength(255)]
+		public string Email { get; set; }
+
+		[MaxLength(20)]
+		public string Phone { get; set; }
+	}
+}
